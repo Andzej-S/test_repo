a191f009d4f3c3e9c27204aba2dd6b1fffff425a
georgeb <george.boholteanu@gmail.com>
2023-02-01 19:31:58 +0000

    js functionality

--- assets/script.js
+++ assets/script.js
@@ -1,100 +1,42 @@
-//https://the-trivia-api.com/api/questions?categories=arts_and_literature&limit=5&difficulty=medium
+let index = 0;
+let limit = 4;
+let queryURL = `https://the-trivia-api.com/api/questions?limit=${limit}&categories=science,history`;
 
-var catagory = "history";
-var limit = "5";
-var difficulty = "medium";
-var quiz;
-var questionNumber = 0;
-var livesAmount = 3;
-var main = document.getElementById('main');
-var questions = document.getElementById('questions');
-var lives = document.getElementById('lives');
-
-$("#button1").on("click", function() {
-    var queryURL = "https://the-trivia-api.com/api/questions?categories=" + catagory + "&limit=" + limit + "&difficulty=" + difficulty;
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function(response) {
-        quiz = response;
-        console.log(response);
-        $("#button1").html("Quiz Started!");
-        if(main.classList.contains('hide')){
-            main.classList.remove('hide');
-        }
-        else
-        {
-            main.classList.add('hide')
-        }
-        // hide main div, show questions
-        DisplayQuestion();
-      });
-      $("#button2").on("click", function() {
-        console.log("The answer to the question is : " + quiz[questionNumber].correctAnswer);
-        if (questionNumber >= quiz.length){
-            console.log("quiz finished");
-        }
-    })
-
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function (response) {
+  $("#button1").on("click", function () {
+    index++;
+    $(".question").text("");
+    $(".answer").text("");
+    drawQuestion(response, index);
+  });
+  console.log(response);
+  drawQuestion(response, index);
 });
 
-function DisplayQuestion(){
-    lives.innerHTML = livesAmount;
-    if(questions.classList.contains('hide')){
-        questions.classList.remove('hide');
-    }
-    var allAnswers = [];
-    choices.innerHTML = "";
-    document.getElementById("question-title").innerHTML = quiz[questionNumber].question;
+function drawQuestion(collector, index) {
+  if (index < limit) {
+    $("<h3/>", {
+      class: "question",
+      html: collector[index].question
+    }).appendTo("body");
 
-    // loop through each incorrect answer and add to array
-    for (var i = 0; i < quiz[questionNumber].incorrectAnswers.length; i++){
-        allAnswers.push(quiz[questionNumber].incorrectAnswers[i]); // push incorrect answers to array
-    }
-
-    allAnswers.push(quiz[questionNumber].correctAnswer); // push correct answer to array
+    let allAnswers = [];
+    allAnswers = [...collector[index].incorrectAnswers];
+    allAnswers.push(collector[index].correctAnswer);
     allAnswers.sort(() => Math.random() - 0.5);
-    
-    for (var i = 0; i < allAnswers.length; i++){
-        var choiceBtn = document.createElement("button");
-        choiceBtn.textContent = allAnswers[i];
-        choices.appendChild(choiceBtn);
-        if(quiz[questionNumber].correctAnswer == allAnswers[i]){
-            $(choiceBtn).on("click", function() {
-                CorrectAnswer();
-                NextQuestion();
-            })
-        }
-        else{
-            $(choiceBtn).on("click", function() {
-                WrongAnswer();
-            })
-        }
-    }
-}
 
-function CorrectAnswer(){
-    alert("Correct!");
-}
+    allAnswers.forEach(function (element) {
+      $("<h5/>", {
+        class: "answer",
+        html: element
+      }).appendTo("body");
+    });
 
-
-function WrongAnswer(){
-    alert("Incorrect!");
-    livesAmount--;
-    lives.innerHTML = livesAmount;
-}
-
-function NextQuestion(){
-    questionNumber++
-    if(questionNumber < limit){
-        DisplayQuestion();
-    }
-    else
-        EndQuiz();
-    }
-
-function EndQuiz(){
-    //hide questions and answers, show hidden divs
+    console.log(allAnswers);
+  } else {
+    console.log("Questions finished");
+  }
 }
